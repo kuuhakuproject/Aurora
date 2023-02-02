866600cd4ca0684c03079c65fa0a28455b3cceac
Davi GC <42722614+DaviGCD@users.noreply.github.com>
2023-02-02 16:34:21 -0300

    feat: Add EmbedBuilder.js

---
+++ src/utils/structures/EmbedBuilder.js
@@ -0,0 +1,33 @@
+export class MessageEmbed {
+  constructor() {
+    this.title =  null
+    this.color = null
+    this.default_color = {
+      DEFAULT: '#C90606',
+      ERROR: '#FF6B08',
+      WARNING: '#FFD208'
+    }
+    this.description = null
+    this.fields = []
+  }
+  
+  setTitle(title) {
+    this.title = title
+  }
+
+  setColor(color) {
+    this.color = typeof color === 'string' ? parseInt(color.replace('#', '0x'), 16) : parseInt(color, 16)
+  }
+  
+  setDefaultColor(color) {
+    this.setColor(this.default_color(color))
+  }
+  
+  setDescription(description) {
+    this.description = description
+  }
+  
+  addField(name, value, inline = false) {
+    this.fields.push({name, value, inline})
+  }
+}
