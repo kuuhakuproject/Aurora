7c6446f9587b1fbd452c9f812784e43caaf79fdd
Davi GC <42722614+DaviGCD@users.noreply.github.com>
2023-02-04 18:14:31 -0300

    feat: Create EmojiListUtils.js

---
+++ src/utils/structures/EmojiListUtils.js
@@ -0,0 +1,7 @@
+export const Emoji = {
+  heatbeat_ping: '<:heatbeat_ping:1029211115667861576>',
+  vps_cpu: '<:vps_cpu:1029215808968200312>',
+  vps_ram: '<:vps_ram:1029214699079876608>',
+  technologist: '🧑‍💻',
+  default_calendar: '<:default_calendar:1029216063898001508>'
+}
