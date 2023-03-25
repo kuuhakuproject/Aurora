6f3f42f611fc779777ff746c64079feb2c6e387f
Davi GC <42722614+DaviGCD@users.noreply.github.com>
2023-03-25 17:12:13 -0300

    Update index.js

--- src/utils/structures/index.js
+++ src/utils/structures/index.js
@@ -1,7 +1,7 @@
-import { Command } from './commands/Command'
-import { CommandContext } from './commands/CommandContext'
-import { Emoji } from './EmojiListUtils'
-import { MessageEmbed } from './EmbedBuilder'
+import { Command } from './commands/Command.js'
+import { CommandContext } from './commands/CommandContext.js'
+import { Emoji } from './EmojiListUtils.js'
+import { MessageEmbed } from './EmbedBuilder.js'
 export {
   Command,
   CommandContext,
