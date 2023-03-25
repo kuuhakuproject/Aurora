6bde983279576b3b89321de765c11fc3f898278d
Davi GC <42722614+DaviGCD@users.noreply.github.com>
2023-03-25 17:08:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import 'dotenv/config'
 import { ShardingManager } from 'discord.js'
 const client = new ShardingManager('./src/AuroraLauncher.js', {
-    totalShards: 2,
-    respawn: true,
-    token: process.env.DISCORD_BOT_TOKEN
+  totalShards: 2,
+  respawn: true,
+  token: process.env.DISCORD_BOT_TOKEN
 })
 
 client.spawn()
