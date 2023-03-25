ddda487a0fc2b4aebb4449b2814863397a278948
Davi GC <42722614+DaviGCD@users.noreply.github.com>
2023-03-25 17:16:57 -0300

    Update ReadyListener.js

--- src/listeners/ReadyListener.js
+++ src/listeners/ReadyListener.js
@@ -2,15 +2,23 @@ import { ApplicationCommandType } from 'discord.js'
 export default {
   label: 'ready',
   execute: function execute(client) {
+    function random_status(time) {
+      const presence = [
+        { name: 'Use /help to see my commands', type: 0 },
+        { name: 'against Angel on chess!' }
+      ]
+      setInterval(function() {
+        const status = presence[Math.round(Math.random() * presence.length)]
+        if (status !== undefined) {
+          client.user.setPresence({ activities: [status] })
+        }
+      }, parseInt(time) * 1000)
+    }
+    
+    random_status(process.env.PRESENCE_COOLDOWN)
     client.commands.forEach((data) => {
-      client.application.commands.create({
-        name: data.name,
-        description: data.description,
-        options: data.options ?? [],
-        type: ApplicationCommandType.ChatInput,
-        defaultMembersPermissions: data.permissions ?? []
-      })
+      client.application.commands.create(data)
     })
-    console.log(`yay, ${client.user.username} is now online!`)
+    console.log(`Everything sounds good! ${client.user.username} is now online!`)
   }
 }
