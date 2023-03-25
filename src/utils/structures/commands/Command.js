99c39e928294654d9703e072138962571d496c76
Davi GC <42722614+DaviGCD@users.noreply.github.com>
2023-03-25 17:15:33 -0300

    Update Command.js

--- src/utils/structures/commands/Command.js
+++ src/utils/structures/commands/Command.js
@@ -2,6 +2,8 @@ export class Command {
   constructor(data) {
     this.name = data.name
     this.description = data.description
+    this.dmPermission = data.dmPermission || false
+    this.type = data.type || 1
     this.options = data.options || []
     this.defaultMembersPermissions = data.permissions || []
   }
