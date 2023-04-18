56316042ce637755f7fcdcac27a4c5b6c64bf7de
Davi GC <42722614+DaviGCD@users.noreply.github.com>
2023-04-18 17:27:19 -0300

    Update HelpCommand.js

--- src/commands/utils/HelpCommand.js
+++ src/commands/utils/HelpCommand.js
@@ -2,15 +2,23 @@ import { Command, MessageEmbed } from '../../utils/structures/index.js'
 export default class HelpCommand extends Command {
   constructor() {
     super({
-      name: 'help',
-      description: 'Shows my commands to you.'
+      data: {
+        name: 'help',
+        description: 'Shows my commands to you.'
+      },
+      category: 'utils'
     })
   }
   
   execute(ctx) {
     const command = ctx.client.application.commands.cache.map(command => `</${command.name}:${command.id}> | ${command.description}`)
+    const embed = new MessageEmbed()
+    embed.setDefaultColor('DEFAULT')
+    embed.setDescription(command.join('\n'))
+    embed.addField('Support server', 'Would you like to join my [support server](https://discord.gg/tPWPz3E3d2)? I would love to see you there! You can talk with my staff team, send memes, talk about anime and much more?!')
     ctx.send({
-      content: `${command.join('\n')}`,
+      content: 'Here it is my command menu with all my commands!',
+      embeds: [embed.build()],
       ephemeral: true
     })
   }
